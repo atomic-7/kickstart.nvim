ba14d8a83916f79add8e8f38d30d3f7f4a31eb1f
atomic <atomic@mpys.de>
2025-06-29 15:33:26 +0200

    neopywal config

--- init.lua
+++ init.lua
@@ -726,10 +726,12 @@ require('lazy').setup({
         },
       }
 
+      -- THEME
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'neopywal'
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,13 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+-- Space for own plugins
+return {
+  {
+    'RedsXDD/neopywal.nvim',
+    name = 'neopywal',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      vim.cmd.colorscheme 'neopywal'
+    end,
+  },
+}
