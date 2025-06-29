9e7c6d789c1bc757555e69f09904118b04b67017
atomic <atomic@mpys.de>
2025-06-29 15:18:36 +0200

    added jj escape in custom keybinds

---
+++ lua/custom/plugins/keybinds.lua
@@ -0,0 +1,7 @@
+-- Keybinds
+-- vim.keymap is only available for neovim 0.7.0+
+local M = {}
+-- TODO: make sure this keybind is only constrained to insert mode in editing and not in telescope and other pickers
+vim.keymap.set('i', 'jj', '<ESC>', { desc = 'Exit insert mode', noremap = true })
+
+return M
